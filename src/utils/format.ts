a885133f1b98909efd771b829e022bf459e12b41
Jiahao Li <lijiahao_shandong@163.com>
2021-12-17 19:09:34 +0800

    feat: format json string

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,9 +1,72 @@
+const INDENT_CHARS = '    ';
+
 export const formatJSON = (jsonString: string | null) => {
   if (!jsonString) return '';
 
-  return jsonString
-          .replace(/\{/g, '{\n')
-          .replace(/\}/g, '\n}')
-          .replace(/\,/g, ',\n')
-          .replace(/\:/g, ': ');
+  let i = 0,
+    result = '',
+    indentLevel = 0,
+    inString = false,
+    currentChar = null;
+
+  for (i = 0; i < jsonString.length; i += 1) {
+    currentChar = jsonString.charAt(i);
+
+    switch (currentChar) {
+      case '{':
+      case '[':
+        if (!inString) {
+          result += currentChar + '\n' + repeat(INDENT_CHARS, indentLevel + 1);
+          indentLevel += 1;
+        } else {
+          result += currentChar;
+        }
+        break;
+      case '}':
+      case ']':
+        if (!inString) {
+          indentLevel -= 1;
+          result += '\n' + repeat(INDENT_CHARS, indentLevel) + currentChar;
+        } else {
+          result += currentChar;
+        }
+        break;
+      case ',':
+        if (!inString) {
+          result += ',\n' + repeat(INDENT_CHARS, indentLevel);
+        } else {
+          result += currentChar;
+        }
+        break;
+      case ':':
+        if (!inString) {
+          result += ': ';
+        } else {
+          result += currentChar;
+        }
+        break;
+      case ' ':
+      case '\n':
+      case '\t':
+        if (inString) {
+          result += currentChar;
+        }
+        break;
+      case '"':
+        if (i > 0 && jsonString.charAt(i - 1) !== '\\') {
+          inString = !inString;
+        }
+        result += currentChar;
+        break;
+      default:
+        result += currentChar;
+        break;
+    }
+  }
+
+  return result;
+};
+
+const repeat = (char: string, count: number) => {
+  return new Array(count + 1).join(char);
 };
--- tests/format.test.ts
+++ tests/format.test.ts
@@ -1,17 +1,37 @@
 import { formatJSON } from '../src/utils/format';
 
+const DEFINITIONS = {
+  data: 'data',
+  definitions: [],
+  type: 'object',
+};
+
 describe('Format test', () => {
   test('Should get empty string When call formatJSON Given empty string', () => {
     expect(formatJSON('')).toBe('');
   });
 
   test('Should get formatted string When call formatJSON Given json string', () => {
-    const jsonString = JSON.stringify({
-      data: 'data',
-      definitions: [],
-      type: 'object',
-    });
+    const jsonString = JSON.stringify(DEFINITIONS);
 
-    expect(formatJSON(jsonString)).toBe(`{\n"data": "data",\n"definitions": [],\n"type": "object"\n}`);
+    expect(formatJSON(jsonString)).toBe(
+      `{\n    "data": "data",\n    "definitions": [\n        \n    ],\n    "type": "object"\n}`
+    );
+  });
+
+  test('Should get formatted string When call formatJSON Given array string in json string', () => {
+    const jsonString = JSON.stringify({ ...DEFINITIONS, definitions: '["definition"]' });
+
+    expect(formatJSON(jsonString)).toBe(
+      `{\n    "data": "data",\n    "definitions": "[\\"definition\\"]",\n    "type": "object"\n}`
+    );
+  });
+
+  test('Should get formatted string When call formatJSON Given , & : in json string', () => {
+    const jsonString = JSON.stringify({ ...DEFINITIONS, definitions: ', test', extra: ': test' });
+
+    expect(formatJSON(jsonString)).toBe(
+      `{\n    "data": "data",\n    "definitions": ", test",\n    "type": "object",\n    "extra": ": test"\n}`
+    );
   });
 });
