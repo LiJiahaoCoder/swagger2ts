db548d719f6a52e46cf26a20fae7f8717db90c5a
Jiahao Li <lijiahao_shandong@163.com>
2021-12-17 23:23:26 +0800

    feat: click header to jump to GitHub

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,9 +1,9 @@
 const PageHeader = () => {
   return (
-    <header className="shadow bg-secondary bg-opacity-75 text-white text-center fs-2 p-2">
-      Swagger2TypeScript
-      <a href="https://github.com/LiJiahaoCoder" target="_blank">
-        <i className="fab fa-github text-white ms-2" />
+    <header className="shadow bg-secondary bg-opacity-75 text-center fs-2 p-2">
+      <a className="text-decoration-none text-white" href="https://github.com/LiJiahaoCoder" target="_blank">
+        Swagger2TypeScript
+        <i className="fab fa-github ms-2" />
       </a>
     </header>
   );
