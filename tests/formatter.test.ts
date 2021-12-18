c3f7f956c421270d6973cc2e5c2d1ba6301d8d00
Jiahao Li <lijiahao_shandong@163.com>
2021-12-18 22:48:30 +0800

    refactor: extract expected vars

--- tests/formatter.test.ts
+++ tests/formatter.test.ts
@@ -13,25 +13,29 @@ describe('Formatter test', () => {
 
   test('Should get formatted string When call formatJSON Given json string', () => {
     const jsonString = JSON.stringify(DEFINITIONS);
+    const expected = `{\n    "data": "data",\n    "definitions": [\n        \n    ],\n    "type": "object"\n}`;
 
-    expect(formatJSON(jsonString)).toBe(
-      `{\n    "data": "data",\n    "definitions": [\n        \n    ],\n    "type": "object"\n}`
-    );
+    expect(formatJSON(jsonString)).toBe(expected);
   });
 
   test('Should get formatted string When call formatJSON Given array string in json string', () => {
-    const jsonString = JSON.stringify({ ...DEFINITIONS, definitions: '["definition"]' });
+    const jsonString = JSON.stringify({
+      ...DEFINITIONS,
+      definitions: '["definition"]',
+    });
+    const expected = `{\n    "data": "data",\n    "definitions": "[\\"definition\\"]",\n    "type": "object"\n}`;
 
-    expect(formatJSON(jsonString)).toBe(
-      `{\n    "data": "data",\n    "definitions": "[\\"definition\\"]",\n    "type": "object"\n}`
-    );
+    expect(formatJSON(jsonString)).toBe(expected);
   });
 
   test('Should get formatted string When call formatJSON Given , & : in json string', () => {
-    const jsonString = JSON.stringify({ ...DEFINITIONS, definitions: ', test', extra: ': test' });
+    const jsonString = JSON.stringify({
+      ...DEFINITIONS,
+      definitions: ', test',
+      extra: ': test',
+    });
+    const expected = `{\n    "data": "data",\n    "definitions": ", test",\n    "type": "object",\n    "extra": ": test"\n}`;
 
-    expect(formatJSON(jsonString)).toBe(
-      `{\n    "data": "data",\n    "definitions": ", test",\n    "type": "object",\n    "extra": ": test"\n}`
-    );
+    expect(formatJSON(jsonString)).toBe(expected);
   });
 });
