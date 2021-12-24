09c9822c862a5db3797ed6d1a8a31536ff0f31b5
Jiahao Li <lijiahao_shandong@163.com>
2021-12-24 18:53:47 +0800

    feat: transfer name to big camel case

---
+++ tests/utils.test.ts
@@ -0,0 +1,15 @@
+import { transferToBigCamelCase } from '@/utils';
+
+describe('Utils test', () => {
+  test('Should get A When call transferToBigCamelCase Given a', () => {
+    expect(transferToBigCamelCase('a')).toBe('A');
+  });
+
+  test('Should get BigCamelCase When call transferToBigCamelCase Given bigCamelCase', () => {
+    expect(transferToBigCamelCase('bigCamelCase')).toBe('BigCamelCase');
+  });
+
+  test('Should get BigCamelCase When call transferToBigCamelCase Given big-camel-Case', () => {
+    expect(transferToBigCamelCase('big-camel-Case')).toBe('BigCamelCase');
+  });
+});
